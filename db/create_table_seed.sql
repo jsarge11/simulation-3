231a620aa2938cf38bfd1e97e1f81bf6a7fe2e41
Jay Sargent <sargentjay11@gmail.com>
2018-04-20 10:24:34 -0600

    step 1 besides nav not appearing on auth

---
+++ db/create_table_seed.sql
@@ -0,0 +1,12 @@
+create table if not exists helo_users (
+    user_id serial primary key,
+    user_display_name text,
+    user_password text,
+    img text
+);
+create table if not exists helo_posts (
+    post_id serial primary key,
+    post_title text,
+    img text,
+    post_content text
+)
